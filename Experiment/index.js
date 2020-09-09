b3b4668653eb7540ee8455b4de7f539a7785a8c2
albertshkhyan <50060559+albertshkhyan@users.noreply.github.com>
2020-09-09 21:57:15 +0400

    learn 4 points, and etc

---
+++ Experiment/index.js
@@ -0,0 +1,19 @@
+{/* javascript & xml (html) <h1></h1> */}
+
+// let h1 = document.createElement("h1");
+// document.body.appendChild(h1)
+
+// const React = {
+//     createElement: function(elem) {
+//         let tag = document.createElement(elem);
+//     }
+// }
+
+
+// class Auto {
+
+// }
+
+
+const x = 10;
+const y = `<h1>${x}</h1`;
