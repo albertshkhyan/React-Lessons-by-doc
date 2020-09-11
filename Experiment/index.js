d0bb6c3fe0d646271ba35a1d9a2c1b59c7d5fd1d
albertshkhyan <50060559+albertshkhyan@users.noreply.github.com>
2020-09-11 22:55:04 +0400

    state, setSate, lifecycle (componentDidMount, componentWilUnmount), HOME WORK 3 and etc

--- Experiment/index.js
+++ Experiment/index.js
@@ -1,4 +1,4 @@
-{/* javascript & xml (html) <h1></h1> */}
+{/* javascript & xml (html) <h1></h1> */ }
 
 // let h1 = document.createElement("h1");
 // document.body.appendChild(h1)
@@ -15,5 +15,26 @@
 // }
 
 
-const x = 10;
-const y = `<h1>${x}</h1`;
+// const x = 10;
+// const y = `<h1>${x}</h1`;
+
+
+//  INCHE E TIMERY HAMARVUM SIDE EFFEKT- vorevhetev timery ashxatum e asinxron (brauzerna ashxatcnum)
+// setTimeout(() => {
+//     console.log(1);
+// })
+
+// console.log(2);
+
+// setTimeout(() => {
+//     console.log(3);
+// })
+//2,1,3
+
+let stop = setInterval(() => {
+    console.log(new Date().toLocaleTimeString());
+}, 1000);
+
+setTimeout(() => {
+    clearInterval(stop);
+}, 5000)
